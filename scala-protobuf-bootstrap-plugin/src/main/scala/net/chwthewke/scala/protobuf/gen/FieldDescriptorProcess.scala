41a4db3c437ede09fcc8069881c2df57f3bcce36
Thomas Dufour <td@thomasdufour.fr>
2013-10-02 20:10:38 +0200

    Fix broken trait with private abstracts

--- scala-protobuf-bootstrap-plugin/src/main/scala/net/chwthewke/scala/protobuf/gen/FieldDescriptorProcess.scala
+++ scala-protobuf-bootstrap-plugin/src/main/scala/net/chwthewke/scala/protobuf/gen/FieldDescriptorProcess.scala
@@ -24,19 +24,19 @@ trait FieldDescriptorProcess {
     case (TYPE_DOUBLE, _) => DoubleClass
     case (TYPE_ENUM, Some(typeName)) => enum(typeName)
     case (TYPE_FIXED32, _) | (TYPE_INT32, _) |
-         (TYPE_SFIXED32, _) | (TYPE_SINT32, _) |
-         (TYPE_UINT32, _) => IntClass
+      (TYPE_SFIXED32, _) | (TYPE_SINT32, _) |
+      (TYPE_UINT32, _) => IntClass
     case (TYPE_FIXED64, _) | (TYPE_INT64, _) |
-         (TYPE_SFIXED64, _) | (TYPE_SINT64, _) |
-         (TYPE_UINT64, _) => LongClass
+      (TYPE_SFIXED64, _) | (TYPE_SINT64, _) |
+      (TYPE_UINT64, _) => LongClass
     case (TYPE_FLOAT, _) => FloatClass
     case (TYPE_GROUP, _) | (TYPE_MESSAGE, _) =>
-      self.typeName match {case Some(typeName) => message(typeName)}
+      self.typeName match { case Some(typeName) => message(typeName) }
     case (TYPE_STRING, _) => StringClass
   }
 
-  private def enum(typeName: String): Symbol
+  def enum(typeName: String): Symbol
 
-  private def message(typeName: String): Symbol
+  def message(typeName: String): Symbol
 
 }
