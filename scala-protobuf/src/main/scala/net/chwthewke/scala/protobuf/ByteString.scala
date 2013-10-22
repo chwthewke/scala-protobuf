4b1bd8535f3ba2d830049a01a7dde95690d4bf04
Thomas Dufour <td@thomasdufour.fr>
2013-10-22 17:43:24 +0200

    Include copy of akka.util.ByteString

--- scala-protobuf/src/main/scala/net/chwthewke/scala/protobuf/ByteString.scala
+++
@@ -1,5 +0,0 @@
-package net.chwthewke.scala.protobuf
-
-class ByteString {
-
-}
--- scala-protobuf/src/main/scala/net/chwthewke/scala/protobuf/package.scala
+++ scala-protobuf/src/main/scala/net/chwthewke/scala/protobuf/package.scala
@@ -4,4 +4,6 @@ import scala.language.existentials
 
 package object protobuf {
   type FieldUpdate[M] = Field[x, y, M]#Update forSome { type x; type y }
+
+  type ByteString = akka.util.ByteString
 }
